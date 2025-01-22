b839d050ee5a01eeb57a778fe8f850119b6a1661
ctangam <luochenjoey@gmail.com>
2025-01-22 17:24:01 +0800

    assembler

--- src/assembler/mod.rs
+++ src/assembler/mod.rs
@@ -1,10 +1,11 @@
 use crate::instruction::Opcode;
 
 pub mod opcode_parsers;
+pub mod register_parsers;
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Token {
     Op(Opcode),
     Register(u8),
-    
+
 }
---
+++ src/assembler/register_parsers.rs
@@ -0,0 +1,27 @@
+use nom::{character::complete::digit1, combinator::map_res, bytes::complete::tag, IResult};
+
+use super::Token;
+
+pub fn register_parsers(i: &[u8]) -> IResult<&[u8], Token> {
+    let (i, _) = tag("$")(i)?;
+    let (i, reg_num) = map_res(digit1, |digit_str: &[u8]| {
+        std::str::from_utf8(digit_str)
+            .unwrap()
+            .parse::<u8>()
+    })(i)?;
+    Ok((i, Token::Register(reg_num)))
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_register() {
+        let result = register_parsers(b"$0");
+        assert!(result.is_ok());
+        let result = register_parsers(b"0");
+        assert!(result.is_err());
+        let result = register_parsers(b"$a");
+        assert!(result.is_err());
+    }
+}
